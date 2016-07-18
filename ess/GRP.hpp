19bc153630d447eddf3e4a653507ee982b5a305a
Dan F-M <danfm@nyu.edu>
2016-07-18 14:36:24 +0200

    adding licenses

--- ess/GRP.hpp
+++ ess/GRP.hpp
@@ -1,6 +1,66 @@
 // GRP.hpp
 // Created by Sivaram Ambikasaran on September 2nd, 2014
 // Modifications by Dan Foreman-Mackey
+//
+// Original license:
+//
+// Copyright © 2014 New York University.
+// All Rights Reserved.
+//
+// A license, solely to non-commercial parties, to use and copy this software and
+// its documentation solely for your non-commercial purposes, without fee and
+// without a signed licensing agreement, is hereby granted upon your download of
+// the software, through which you agree to the following: 1) the above copyright
+// notice, this paragraph and the following three paragraphs will prominently
+// appear in all internal copies and modifications; 2) no rights to sublicense or
+// further distribute this software for commercial purposes are granted; and 3) no
+// rights to assign this license are granted. You may modify or otherwise generate
+// derivatives of the software, provided that such derivatives are for
+// non-commercial purposes only. The use of all such derivatives shall remain
+// subject to the terms contained herein. Please contact The Office of Industrial
+// Liaison, New York University, One Park Avenue, 6th Floor, New York, NY 10016,
+// (212) 263-8178, for commercial licensing opportunities, or for further
+// distribution, modification or license rights.
+//
+// Created by Sivaram Ambikasaran.
+//
+// IN NO EVENT SHALL NYU, OR ITS EMPLOYEES, OFFICERS, AGENTS OR TRUSTEES
+// (“COLLECTIVELY “NYU PARTIES”) BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES OF ANY KIND, INCLUDING LOST
+// PROFITS, ARISING OUT OF ANY CLAIM RESULTING FROM YOUR USE OF THIS SOFTWARE
+// AND ITS DOCUMENTATION, EVEN IF ANY OF NYU PARTIES HAS BEEN ADVISED OF THE
+// POSSIBILITY OF SUCH CLAIM OR DAMAGE.
+//
+// NYU SPECIFICALLY DISCLAIMS ANY WARRANTIES OF ANY KIND REGARDING THE SOFTWARE,
+// INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, OR THE ACCURACY OR
+// USEFULNESS, OR COMPLETENESS OF THE SOFTWARE. THE SOFTWARE AND ACCOMPANYING
+// DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED COMPLETELY "AS IS".
+// NYU HAS NO OBLIGATION TO PROVIDE FURTHER DOCUMENTATION, MAINTENANCE,
+// SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+//
+//
+// Please cite Generalized Rybicki Press algorithm and the code, if you use the
+// code, ESS, in your research.
+//
+// Citation for the article:
+//
+// @article{ambikasaran2014generalized,
+//   title={Generalized Rybicki Press algorithm},
+//   author={Ambikasaran, Sivaram},
+//   journal={arXiv preprint arXiv:1409.7852},
+//   year={2014}
+// }
+//
+// Citation for the code:
+//
+// @MISC{ESSweb,
+//   author = {Sivaram Ambikasaran},
+//   title = {ESS},
+//   howpublished = {https://github.com/sivaramambikasaran/ESS},
+//   year = {2014}
+//  }
+//
 
 #ifndef __GRP_HPP__
 #define __GRP_HPP__
