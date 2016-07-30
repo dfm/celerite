09ccbe7476b28355315a2d3abe4e9434105b720f
Dan F-M <danfm@nyu.edu>
2016-07-29 23:14:47 -0700

    better benchmarking

--- python/demo.py
+++ python/demo.py
@@ -15,8 +15,8 @@ gp = GP()
 gp.add_term(1.0, 0.1)
 gp.add_term(0.1, 2.0, 1.6)
 
-K = 3
 N = 2**np.arange(5, 20)
+K = np.maximum((N.max() / N), 5*np.ones_like(N)).astype(int)
 times = np.empty((len(N), 2))
 
 t = np.sort(np.random.rand(np.max(N)))
@@ -25,25 +25,25 @@ y = np.sin(t)
 
 for i, n in enumerate(N):
     strt = time.time()
-    for k in range(K):
+    for k in range(K[i]):
         gp.compute(t[:n], yerr[:n])
-    times[i, 0] = (time.time() - strt) / K
+    times[i, 0] = (time.time() - strt) / K[i]
 
     strt = time.time()
-    for k in range(K):
+    for k in range(K[i]):
         gp.log_likelihood(y[:n])
-    times[i, 1] = (time.time() - strt) / K
+    times[i, 1] = (time.time() - strt) / K[i]
 
     print(n, times[i, 0], times[i, 1])
 
-pl.plot(N, N / N[0] * 1e-3, "k", label="$\mathcal{O}(N)$")
+pl.plot(N, N / N[-1] * 2.0, "k", label="$\mathcal{O}(N)$")
 pl.plot(N, times[:, 0], ".-", label="compute")
 pl.plot(N, times[:, 1], ".-", label="log likelihood")
 pl.xscale("log")
 pl.yscale("log")
 pl.legend(loc=2, fontsize=15)
 pl.xlim(N.min(), N.max())
-pl.ylim(3e-5, 15)
+pl.ylim(1e-5, 3.0)
 pl.xlabel("number of data points")
 pl.ylabel("computational cost [seconds]")
 pl.savefig("demo.png", dpi=300, bbox_inches="tight")
