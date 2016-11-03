45ec50fce00d33c583fd269133529f76a574deb3
Dan F-M <danfm@nyu.edu>
2016-11-03 15:30:11 -0700

    cleaning up a bit

---
+++ paper/approximate_kernels/gaussian_approx4.jl
@@ -0,0 +1,99 @@
+# Try to approximate squared-exponential kernel:
+using PyPlot
+using Optim
+
+function gaussian_approx4()
+
+ntime = 1000
+time = collect(linspace(0,10,ntime))
+gaussian=exp(-time.^2/2.)
+
+function compute_chi(x)
+model = zeros(ntime)
+c1 = 1.0
+c2 = 0.0
+c3 = 1.0
+for k=1:nexp
+  model += x[1+(k-1)*3].*exp(-x[2+(k-1)*3].*time).*cos(x[3+(k-1)*3].*time)
+  c1 -= x[1+(k-1)*3]
+  c2 -= x[1+(k-1)*3]*x[2+(k-1)*3]
+  c3 += x[1+(k-1)*3]*(x[2+(k-1)*3]^2-x[3+(k-1)*3]^2)
+end
+c2 = c2/c1
+c3 = sqrt(abs((c3+c1*c2^2)/c1))
+#model += c1.*exp(-c2.*time).*cos(x[nexp*3+1].*time)
+model += c1.*exp(-c2.*time).*cos(c3.*time)
+return sum((gaussian-model).^2)
+end  
+
+nexp = 3
+#nexp = 2
+#nexp = 4
+npar = 3*nexp
+abest=zeros(npar)
+#abest[1:12]=[1./6.,0.1,sqrt(3.),1./6.,0.1,sqrt(5.),1./3.,0.1,0.1,1./3.,0.1,0.2]
+# Here are best parameters so far:
+#New minimum: 855 2.522483789529161 [0.09778291672462784,1.5548632770098678,0.32192744041675075,0.596959899332403,0.3132368383113517,-0.18163977156887445,0.2670528045472698,-1.075894228060323]
+#abest=[0.09778291672462784,1.5548632770098678,0.32192744041675075,0.596959899332403,0.3132368383113517,-0.18163977156887445,0.2670528045472698,-1.075894228060323]
+#New minimum: 4.2559231434310284e-7 [1.4707280328402002,1.3556988707754538,1.9133776307579318,-0.7331091089111925,3.6344847969483203,1.3207054708438177,-2.5676669507901395,1.4654679150660068,-1.8772555559415482,2.8301497749105042,1.5683246010038931,0.32031045862921226] 2.0325193117951805e-5
+#abest=[1.4707280328402002,1.3556988707754538,1.9133776307579318,-0.7331091089111925,3.6344847969483203,1.3207054708438177,-2.5676669507901395,1.4654679150660068,-1.8772555559415482,2.8301497749105042,1.5683246010038931,0.32031045862921226]
+#New minimum: 6.819157755054511e-6 [2.7518074963750734,1.5312499407621147,-0.34148888178746056,-0.3546256365636662,4.753504292338284,2.042571639590954,-1.3968344706386124,1.7967431129812566,-1.7641684073510928] 8.113071479846436e-5 0.00034738917279475423
+abest=[2.7518074963750734,1.5312499407621147,-0.34148888178746056,-0.3546256365636662,4.753504292338284,2.042571639590954,-1.3968344706386124,1.7967431129812566,-1.7641684073510928]
+#abest=[2.7518074963750734,1.5312499407621147,-0.34148888178746056,-0.3546256365636662,4.753504292338284,2.042571639590954,-1.7641684073510928]
+#abest=[2.7518074963750734,1.5312499407621147,-0.34148888178746056,-0.3546256365636662,4.753504292338284,2.042571639590954]
+#abest=[2.7518074963750734,1.5312499407621147,-0.34148888178746056,-0.3546256365636662,4.753504292338284,2.042571639590954]
+
+
+atrial = zeros(npar)
+for k=1:npar
+  atrial[k] = abest[k]
+end
+model = zeros(ntime)
+c1 = 1.0
+c2 = 0.0
+c3 = 1.0
+for k=1:nexp-1
+  model += atrial[1+(k-1)*3].*exp(-atrial[2+(k-1)*3].*time).*cos(atrial[3+(k-1)*3].*time)
+  c1 -= atrial[1+(k-1)*3]
+  c2 -= atrial[1+(k-1)*3]*atrial[2+(k-1)*3]
+  c3 += atrial[1+(k-1)*3]*(atrial[2+(k-1)*3]^2-atrial[3+(k-1)*3]^2)
+end
+c2 = c2/c1
+c3 = sqrt((c3+c1*c2^2)/c1)
+model += c1.*exp(-c2.*time).*cos(c3.*time)
+clf()
+plot(time,gaussian)
+plot(time,model)
+#chibest=sum((gaussian-model).^2)
+chibest=compute_chi(atrial)
+
+println("Initial chi-square: ",chibest)
+read(STDIN,Char)
+
+result = optimize(compute_chi, atrial, BFGS(), OptimizationOptions(autodiff = true))
+
+println(Optim.minimizer(result),Optim.minimum(result))
+abest= Optim.minimizer(result)
+clf()
+plot(time,gaussian)
+plot(time,model)
+model = zeros(ntime)
+c1 = 1.0
+c2 = 0.0
+c3 = 1.0
+for k=1:nexp
+  model += abest[1+(k-1)*3].*exp(-abest[2+(k-1)*3].*time).*cos(abest[3+(k-1)*3].*time)
+  c1 -= abest[1+(k-1)*3]
+  c2 -= abest[1+(k-1)*3]*abest[2+(k-1)*3]
+  c3 += abest[1+(k-1)*3]*(abest[2+(k-1)*3]^2-abest[3+(k-1)*3]^2)
+end
+c2 = c2/c1
+c3 = sqrt((c3+c1*c2^2)/c1)
+model += c1.*exp(-c2.*time).*cos(c3.*time)
+plot(time,(gaussian-model))
+chi=sum((gaussian-model).^2)
+println("New minimum: ",chi," ",abest," ",c1," ",c2," ",c3," ",std(gaussian-model)," ",maximum(abs(gaussian-model)))
+read(STDIN,Char)
+
+return
+end
---
+++ paper/approximate_kernels/kernel_approx.jl
@@ -0,0 +1,79 @@
+# Try to approximate squared-exponential kernel:
+using PyPlot
+
+ntime = 1000
+time = collect(linspace(0,10,ntime))
+
+# If we want to approximate Gaussian, then we just get a cosine... that doesn't work well!
+
+# How about sum of two?  This I solved numerically with Mathematically, and unfortunately I find
+# a growing solution in each case... darn.
+
+#nexp = 3
+nexp = 4
+npar = 2*nexp
+abest=zeros(npar)
+#abest[1:6]=[-9.,1.,10.,0.9,0.1,1.1]
+abest[1:6]=[-3.,1.,4.,0.75,0.1,1.1]
+# Here are best parameters so far:
+#New minimum: 7063 0.0009743003566026671 
+#abest=[-4.081065227181365,1.0304513099118429,0.7384172905709343,1.6404128688104038,-0.6973216477808734,0.47273293933522775,5.0399695843913035,0.659463641544771]
+#New minimum: 4091 0.0007528492057342187 
+#abest=[-3.983277629878132,1.0735443595362018,0.9310909822220776,1.609723984179903,-0.728531695566164,0.4833054951112925,4.780718343222218,0.6546148284917492]
+#New minimum: 3509 0.0006919748190206947 [-4.012577986446492,1.0702556640310126,0.9246366188173811,1.6094033319207477,-0.756585292781486,0.48539249997921724,4.844526660410597,0.655092253875969]
+#abest=[-4.012577986446492,1.0702556640310126,0.9246366188173811,1.6094033319207477,-0.756585292781486,0.48539249997921724,4.844526660410597,0.655092253875969]
+#New minimum: 99883 0.0004297805010976177 [-4.707752923722873,1.0471511183944253,1.0707583835332415,1.5530678690438415,-1.023610702073631,0.522600717254369,5.660605242263262,0.6716080388250383]
+#abest=[-4.707752923722873,1.0471511183944253,1.0707583835332415,1.5530678690438415,-1.023610702073631,0.522600717254369,5.660605242263262,0.6716080388250383]
+#New minimum: 99770 0.00034643674752663026 [-5.071392864979707,1.041877623915747,1.1886472038873641,1.521389181893939,-1.1341375315197493,0.5358293183936108,6.016883192612092,0.6786038154302075]
+abest=[-5.071392864979707,1.041877623915747,1.1886472038873641,1.521389181893939,-1.1341375315197493,0.5358293183936108,6.016883192612092,0.6786038154302075]
+#New minimum: 999639 0.00016786630230513215 [-6.52117872187123,1.0277101206108639,1.6861024710175314,1.4296430478894617,-1.5767703086820388,0.5777664250974277,7.4118465595357375,0.7018981839031324]
+#abest=[-6.52117872187123,1.0277101206108639,1.6861024710175314,1.4296430478894617,-1.5767703086820388,0.5777664250974277,7.4118465595357375,0.7018981839031324]
+
+#New minimum: 950075 0.0001478308089395661 [-6.80168281621302,1.0282509070629977,1.8226885846422511,1.4133527852506547,-1.7003897369362933,0.5856387342941094,7.679383968507063,0.7049456900610803]
+
+#abest[1:4]=[-9.,1.,10.1,0.9]
+abest[7]=1.0-abest[1]-abest[3]-abest[5]
+abest[8]=-(abest[1]*abest[2]+abest[3]*abest[4]+abest[5]*abest[6])/abest[7]
+#abest[5]=1.0-abest[1]-abest[3]
+#abest[6]=-(abest[1]*abest[2]+abest[3]*abest[4])/abest[5]
+atrial = zeros(npar)
+for k=1:npar
+  atrial[k] = abest[k]
+end
+model = zeros(ntime)
+for k=1:nexp
+  model += atrial[1+(k-1)*2].*exp(-atrial[2+(k-1)*2].*time)
+end
+clf()
+plot(time,exp(-time.^2./2))
+plot(time,model)
+#chibest=sum(((1.0+time+time.^2./3.0).*exp(-time)-model).^2)
+chibest=sum((exp(-time.^2/2)-model).^2)
+println("Initial chi-square: ",chibest)
+read(STDIN,Char)
+atrial = zeros(npar)
+for i=1:1000000
+  for j=1:npar-2
+    atrial[j] = abest[j] + 0.01*randn()
+    atrial[7]= 1.0-atrial[1]-atrial[3]-atrial[5]
+    atrial[8]=-(atrial[1]*atrial[2]+atrial[3]*atrial[4]+atrial[5]*atrial[6])/atrial[7]
+#    atrial[5]= 1.0-atrial[1]-atrial[3]
+#    atrial[6]=-(atrial[1]*atrial[2]+atrial[3]*atrial[4])/atrial[5]
+    model = zeros(ntime)
+    for k=1:nexp
+      model += atrial[1+(k-1)*2].*exp(-atrial[2+(k-1)*2].*time)
+    end
+    chi=sum((exp(-time.^2/2)-model).^2)
+    if chi < chibest
+      chibest = chi
+      for k=1:npar
+        abest[k] = atrial[k]
+      end
+      println("New minimum: ",i," ",chibest," ",abest)
+      clf()
+      plot(time,exp(-time.^2/2))
+      plot(time,model)
+      plot(time,exp(-time.^2/2)-model)
+    end
+  end
+end
