12e83a790912b0a5c95695243c1598d2a44202cf
Dan F-M <danfm@nyu.edu>
2016-11-07 11:10:11 -0800

    working on CI

---
+++ .ci/travis.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -x
+
+# Download the requested version of Eigen
+mkdir -p eigen
+cd eigen
+wget --quiet "http://bitbucket.org/eigen/eigen/get/${EIGEN_VERSION}.tar.gz"
+tar -xf ${EIGEN_VERSION}.tar.gz --strip-components 1
+cd ..
+
+# If testing C++, deal with that here
+if [[ $TEST_LANG == cpp ]]
+then
+  cd cpp
+  cmake . -DEIGEN_CHECK_INCLUDE_DIRS=../eigen
+  make
+  cd ..
+  return
+fi
+
+# Conda Python
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda info -a
+conda create --yes -n test python=$PYTHON_VERSION numpy=$NUMPY_VERSION Cython setuptools pytest
+source activate test
+
+# Build the extension
+cd python
+CXX=g++ python setup.py build_ext -I../eigen --inplace
+cd ..
