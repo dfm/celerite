bf8641cf7fd5d3b65baf8cb9be98f022e7b9a070
Dan F-M <danfm@nyu.edu>
2016-11-09 17:21:44 -0800

    building the paper on travis

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -7,6 +7,17 @@ wget --quiet "http://bitbucket.org/eigen/eigen/get/${EIGEN_VERSION}.tar.gz"
 tar -xf ${EIGEN_VERSION}.tar.gz --strip-components 1
 cd ..
 
+# If building the paper, do that here
+if [[ $TEST_LANG == paper ]]
+then
+  if git diff --name-only $TRAVIS_COMMIT_RANGE | grep 'paper/'
+  then
+    echo "Building the paper..."
+    source "$( dirname "${BASH_SOURCE[0]}" )"/setup-texlive.sh
+  fi
+  return
+fi
+
 # If testing C++, deal with that here
 if [[ $TEST_LANG == cpp ]]
 then
