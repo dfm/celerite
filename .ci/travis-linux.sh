dd936d7852e8c2422c3ab3bdae1931065650e9ef
Dan F-M <danfm@nyu.edu>
2016-11-09 17:50:58 -0800

    making a change to the paper

--- .ci/travis-linux.sh
+++ .ci/travis-linux.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -x
 
 # Install conda
-# http://conda.pydata.org/docs/travis.html#the-travis-yml-file
-wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
+if [[ $TEST_LANG == python ]]
+then
+  # http://conda.pydata.org/docs/travis.html#the-travis-yml-file
+  wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
+  bash miniconda.sh -b -p $HOME/miniconda
+  export PATH="$HOME/miniconda/bin:$PATH"
+fi
 
 # Install Python dependencies
 source "$( dirname "${BASH_SOURCE[0]}" )"/travis.sh
