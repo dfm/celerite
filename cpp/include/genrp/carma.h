4530e4e2359f442a99013fdb187b4306b04ce1ee
Dan F-M <danfm@nyu.edu>
2016-12-20 13:10:41 -0500

    updating CARMA benchmark comparison

--- cpp/include/genrp/carma.h
+++ cpp/include/genrp/carma.h
@@ -2,11 +2,18 @@
 #define _GENRP_CARMA_H_
 
 #include <cmath>
+#include <cfloat>
 #include <complex>
 #include <Eigen/Dense>
 
 namespace genrp {
 namespace carma {
+
+template <typename T1, typename T2>
+inline bool isclose (const T1& a, const T2& b) {
+  using std::abs;
+  return (abs(a - b) <= 1e-6);
+}
 
 Eigen::VectorXcd roots_from_params (const Eigen::VectorXd& params) {
   unsigned n = params.rows();
@@ -64,6 +71,67 @@ CARMASolver (double log_sigma, Eigen::VectorXd arpars, Eigen::VectorXd mapars)
   alpha_ = poly_from_roots(arroots_);
   beta_ = poly_from_roots(maroots_);
   beta_ /= beta_(0);
+};
+
+void get_genrp_coeffs (
+    Eigen::VectorXd& alpha_real, Eigen::VectorXd& beta_real,
+    Eigen::VectorXd& alpha_complex_real, Eigen::VectorXd& alpha_complex_imag,
+    Eigen::VectorXd& beta_complex_real, Eigen::VectorXd& beta_complex_imag
+) const {
+  alpha_real.resize(p_);
+  beta_real.resize(p_);
+  alpha_complex_real.resize(p_);
+  alpha_complex_imag.resize(p_);
+  beta_complex_real.resize(p_);
+  beta_complex_imag.resize(p_);
+  size_t p_real = 0, p_complex = 0;
+  bool is_conj;
+  std::complex<double> term1, term2, full_term;
+  for (size_t k = 0; k < p_; ++k) {
+    term1 = 0.0;
+    term2 = 0.0;
+    for (size_t l = 0; l < q_ + 1; ++l) {
+      term1 += beta_[l] * pow(arroots_[k], l);
+      term2 += beta_[l] * pow(-arroots_[k], l);
+    }
+    full_term = -sigma_ * sigma_ * term1 * term2 / arroots_[k].real();
+    for (size_t l = 0; l < p_; ++l) {
+      if (l != k)
+        full_term /= (arroots_[l] - arroots_[k]) * (std::conj(arroots_[l]) + arroots_[k]);
+    }
+
+    // Check for and discard conjugate pairs.
+    if (isclose(full_term.imag(), 0.0) && isclose(arroots_[k].imag(), 0.0)) {
+      alpha_real[p_real] = 0.5 * full_term.real();
+      beta_real[p_real] = -arroots_[k].real();
+      p_real ++;
+    } else {
+      is_conj = false;
+      for (size_t l = 0; l < p_complex; ++l) {
+        if (isclose(alpha_complex_real[l], full_term.real()) &&
+            isclose(alpha_complex_imag[l], -full_term.imag()) &&
+            isclose(beta_complex_real[l], -arroots_[k].real()) &&
+            isclose(beta_complex_imag[l], arroots_[k].imag())) {
+          is_conj = true;
+          break;
+        }
+      }
+      if (!is_conj) {
+        alpha_complex_real[p_complex] = full_term.real();
+        alpha_complex_imag[p_complex] = full_term.imag();
+        beta_complex_real[p_complex] = -arroots_[k].real();
+        beta_complex_imag[p_complex] = -arroots_[k].imag();
+        p_complex ++;
+      }
+    }
+  }
+
+  alpha_real = alpha_real.head(p_real);
+  beta_real = beta_real.head(p_real);
+  alpha_complex_real = alpha_complex_real.head(p_complex);
+  alpha_complex_imag = alpha_complex_imag.head(p_complex);
+  beta_complex_real = beta_complex_real.head(p_complex);
+  beta_complex_imag = beta_complex_imag.head(p_complex);
 };
 
 void setup () {
@@ -125,7 +193,7 @@ void advance_time (double dt) {
   state_.P = lam.asDiagonal() * (state_.P - V_) * lam.conjugate().asDiagonal() + V_;
 };
 
-double log_likelihood (Eigen::VectorXd t, Eigen::VectorXd y, Eigen::VectorXd yerr) {
+double log_likelihood (const Eigen::VectorXd& t, const Eigen::VectorXd& y, const Eigen::VectorXd& yerr) {
   unsigned n = t.rows();
   double r, ll = n * log(2.0 * M_PI);
   Prediction pred;
--- cpp/src/carma_comp.cc
+++ cpp/src/carma_comp.cc
@@ -5,6 +5,7 @@
 
 #include "genrp/solvers.h"
 #include "genrp/carma.h"
+#include "genrp/utils.h"
 
 // Timer for the benchmark.
 double get_timestamp ()
@@ -18,91 +19,137 @@ int main (int argc, char* argv[])
 {
   srand(42);
 
-  size_t nterms = 3;
-  if (argc >= 2) nterms = atoi(argv[1]);
-  size_t N_max = pow(2, 19);
-  if (argc >= 3) N_max = atoi(argv[2]);
+  size_t max_terms = 10;
+  if (argc >= 2) max_terms = atoi(argv[1]);
+  size_t N = pow(2, 16);
+  if (argc >= 3) N = atoi(argv[2]);
   size_t niter = 5;
   if (argc >= 4) niter = atoi(argv[3]);
 
   double sigma = 1.0;
-  Eigen::VectorXd carma_arparams = Eigen::VectorXd::Random(nterms),
-                  carma_maparams = Eigen::VectorXd::Random(nterms-1);
-
-  // Set up the coefficients.
-  /* size_t n_complex = nterms / 2, */
-  /*        n_real = nterms - n_complex * 2; */
-  size_t n_complex = 0, n_real = nterms;
-  std::cout << n_real << " " << n_complex << "\n";
-  Eigen::VectorXd alpha_real = Eigen::VectorXd::Random(n_real),
-                  beta_real = Eigen::VectorXd::Random(n_real),
-                  alpha_complex = Eigen::VectorXd::Random(n_complex),
-                  beta_complex_real = Eigen::VectorXd::Random(n_complex),
-                  beta_complex_imag = Eigen::VectorXd::Random(n_complex);
-  if (n_real > 0) {
-    alpha_real.array() += 1.0;
-    beta_real.array() += 1.0;
-  }
-  if (n_complex > 0) {
-    alpha_complex.array() += 1.0;
-    beta_complex_real.array() += 1.0;
-    beta_complex_imag.array() += 1.0;
-  }
 
   // Generate some fake data.
-  Eigen::VectorXd x0 = Eigen::VectorXd::Random(N_max),
-                  yerr0 = Eigen::VectorXd::Random(N_max),
-                  y0;
+  Eigen::VectorXd x = Eigen::VectorXd::Random(N),
+                  yerr = Eigen::VectorXd::Random(N),
+                  y, diag;
+  yerr.array() *= 0.1;
+  yerr.array() += 1.0;
+  diag = yerr.array() * yerr.array();
+  std::sort(x.data(), x.data() + x.size());
+  y = sin(x.array());
 
-  // Set the scale of the uncertainties.
-  yerr0.array() *= 0.1;
-  yerr0.array() += 1.0;
+  double carma_ll, genrp_ll, strt;
+  Eigen::MatrixXd compute_times(max_terms, 3);
+  for (size_t nterms = 1; nterms <= max_terms; ++nterms) {
+    Eigen::VectorXd carma_arparams = Eigen::VectorXd::Random(nterms),
+                    carma_maparams = Eigen::VectorXd::Random(nterms-1),
+                    alpha_real, beta_real, alpha_complex_real, alpha_complex_imag,
+                    beta_complex_real, beta_complex_imag;
 
-  // The times need to be sorted.
-  std::sort(x0.data(), x0.data() + x0.size());
+    compute_times(nterms - 1, 0) = nterms;
 
-  // Compute the y values.
-  y0 = sin(x0.array());
-
-  genrp::BandSolver<double> solver;
-
-  for (size_t N = 64; N <= N_max; N *= 2) {
-    Eigen::VectorXd x = x0.topRows(N),
-                    yerr = yerr0.topRows(N),
-                    y = y0.topRows(N);
-
-    // Benchmark the solver.
-    double strt,
-           compute_time = 0.0, solve_time = 0.0, carma_time = 0.0;
-
+    // Compute using the CARMA model.
+    strt = get_timestamp();
     for (size_t i = 0; i < niter; ++i) {
-      strt = get_timestamp();
-      solver.compute(alpha_real, beta_real, alpha_complex, beta_complex_real, beta_complex_imag, x, yerr);
-      compute_time += get_timestamp() - strt;
-    }
-
-    for (size_t i = 0; i < niter; ++i) {
-      strt = get_timestamp();
-      solver.dot_solve(y);
-      solve_time += get_timestamp() - strt;
-    }
-
-    for (size_t i = 0; i < niter; ++i) {
-      strt = get_timestamp();
       genrp::carma::CARMASolver carma_solver(0.0, carma_arparams, carma_maparams);
       carma_solver.setup();
-      carma_solver.log_likelihood(x, y, yerr);
-      carma_time += get_timestamp() - strt;
+      carma_ll = carma_solver.log_likelihood(x, y, yerr);
     }
+    compute_times(nterms - 1, 1) = (get_timestamp() - strt) / niter;
 
-    // Print the results.
-    std::cout << N;
-    std::cout << " ";
-    std::cout << (compute_time + solve_time) / niter;
-    std::cout << " ";
-    std::cout << carma_time / niter;
-    std::cout << "\n";
+    // Get the GenRP parameters for the CARMA model.
+    genrp::carma::CARMASolver carma_solver(0.0, carma_arparams, carma_maparams);
+    carma_solver.get_genrp_coeffs(alpha_real, beta_real,
+      alpha_complex_real, alpha_complex_imag, beta_complex_real, beta_complex_imag);
+
+    // Compute using the genrp model.
+    genrp::BandSolver<double> solver;
+    strt = get_timestamp();
+    for (size_t i = 0; i < niter; ++i) {
+      solver.compute(alpha_real, beta_real, alpha_complex_real, alpha_complex_imag, beta_complex_real, beta_complex_imag, x, diag);
+      genrp_ll = -0.5*(solver.dot_solve(y) + solver.log_determinant() + x.rows() * log(2.0 * M_PI));
+    }
+    compute_times(nterms - 1, 2) = (get_timestamp() - strt) / niter;
   }
+
+  std::cout<< compute_times << std::endl;
 
   return 0;
 }
+
+//   // Set up the coefficients.
+//   /* size_t n_complex = nterms / 2, */
+//   /*        n_real = nterms - n_complex * 2; */
+//   size_t n_complex = 0, n_real = nterms;
+//   std::cout << n_real << " " << n_complex << "\n";
+//   Eigen::VectorXd alpha_real = Eigen::VectorXd::Random(n_real),
+//                   beta_real = Eigen::VectorXd::Random(n_real),
+//                   alpha_complex = Eigen::VectorXd::Random(n_complex),
+//                   beta_complex_real = Eigen::VectorXd::Random(n_complex),
+//                   beta_complex_imag = Eigen::VectorXd::Random(n_complex);
+//   if (n_real > 0) {
+//     alpha_real.array() += 1.0;
+//     beta_real.array() += 1.0;
+//   }
+//   if (n_complex > 0) {
+//     alpha_complex.array() += 1.0;
+//     beta_complex_real.array() += 1.0;
+//     beta_complex_imag.array() += 1.0;
+//   }
+//
+//   // Generate some fake data.
+//   Eigen::VectorXd x0 = Eigen::VectorXd::Random(N_max),
+//                   yerr0 = Eigen::VectorXd::Random(N_max),
+//                   y0;
+//
+//   // Set the scale of the uncertainties.
+//   yerr0.array() *= 0.1;
+//   yerr0.array() += 1.0;
+//
+//   // The times need to be sorted.
+//   std::sort(x0.data(), x0.data() + x0.size());
+//
+//   // Compute the y values.
+//   y0 = sin(x0.array());
+//
+//   genrp::BandSolver<double> solver;
+//
+//   for (size_t N = 64; N <= N_max; N *= 2) {
+//     Eigen::VectorXd x = x0.topRows(N),
+//                     yerr = yerr0.topRows(N),
+//                     y = y0.topRows(N);
+//
+//     // Benchmark the solver.
+//     double strt, compute_time = 0.0, solve_time = 0.0, carma_time = 0.0;
+//
+//     for (size_t i = 0; i < niter; ++i) {
+//       strt = get_timestamp();
+//       solver.compute(alpha_real, beta_real, alpha_complex, beta_complex_real, beta_complex_imag, x, yerr);
+//       compute_time += get_timestamp() - strt;
+//     }
+//
+//     for (size_t i = 0; i < niter; ++i) {
+//       strt = get_timestamp();
+//       solver.dot_solve(y);
+//       solve_time += get_timestamp() - strt;
+//     }
+//
+//     for (size_t i = 0; i < niter; ++i) {
+//       strt = get_timestamp();
+//       genrp::carma::CARMASolver carma_solver(0.0, carma_arparams, carma_maparams);
+//       carma_solver.setup();
+//       carma_solver.log_likelihood(x, y, yerr);
+//       carma_time += get_timestamp() - strt;
+//     }
+//
+//     // Print the results.
+//     std::cout << N;
+//     std::cout << " ";
+//     std::cout << (compute_time + solve_time) / niter;
+//     std::cout << " ";
+//     std::cout << carma_time / niter;
+//     std::cout << "\n";
+//   }
+//
+//   return 0;
+// }
