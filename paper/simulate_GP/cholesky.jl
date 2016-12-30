5b0429bdbe671249fd3557aa6c61017e5e7de2c5
Eric Agol <agol@uw.edu>
2016-12-30 10:39:17 -0800

    Small corrections; p -> J; p_0 -> J_0

--- paper/simulate_GP/cholesky.jl
+++ paper/simulate_GP/cholesky.jl
@@ -1,8 +1,9 @@
 alpha = rand()+im*rand()
 beta = rand()+im*rand()
+#beta = 0.0+im*rand()
 
 # Compute covariance matrix:
-nt = 11
+nt = 5
 t = linspace(0,nt-1,nt)
 
 cov = zeros(Complex{Float64},nt,nt)
@@ -29,3 +30,39 @@ ccov=chol(cov)
 #end
 #
 #covinvchol=chol(covinv)
+
+# Check that cholesky decomposition is correct:
+*(ctranspose(ccov),ccov)-cov
+
+# Now try constructing:
+a=zeros(Complex64,nt)
+b=zeros(Complex64,nt)
+v=zeros(Complex64,nt)
+matrix = zeros(Complex64,nt,nt)
+for i=1:nt
+  a[i]=sqrt(alpha)*exp(beta*t[i])
+  b[i]=sqrt(alpha)*exp(-beta*t[i])
+  v[i]=b[i]/a[i]
+end
+d = Diagonal(a)
+l = zeros(Complex64,nt,nt)
+lam=zeros(Complex64,nt,nt)
+for i=1:nt
+  for j=1:i
+    l[i,j]=1.0+im*0.0
+  end
+  if i < nt
+    lam[i,i] = sqrt(v[i]-v[i+1])
+  else
+    lam[i,i] = sqrt(v[i])
+  end
+  for j=1:nt
+    if j >= i
+      matrix[i,j] = a[i]*b[j]
+    else
+      matrix[i,j] = a[j]*b[i]
+    end
+  end
+end
+cholm = *(d,*(l,lam))
+
