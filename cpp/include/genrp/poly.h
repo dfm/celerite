ea1f33f88f9a02ebccad11062002420e609d6512
Dan F-M <danfm@nyu.edu>
2017-01-03 07:49:15 -0800

    wtf

--- cpp/include/genrp/poly.h
+++ cpp/include/genrp/poly.h
@@ -61,7 +61,7 @@ Eigen::Matrix<T, Eigen::Dynamic, 1> polyrem (const Eigen::Matrix<T, Eigen::Dynam
   }
   int strt;
   for (strt = 0; strt < m; ++strt) {
-    if (abs(r[strt]) >= T(POLYTOL)) {
+    if (abs(r[strt]) >= 1e-10) {
       return r.tail(m + 1 - strt);
     }
   }
