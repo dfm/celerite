59062c1066f80cff5c47cb97d35ede02c7141015
Dan F-M <danfm@nyu.edu>
2017-01-02 16:33:33 -0800

    does this work, travis?

--- cpp/include/genrp/poly.h
+++ cpp/include/genrp/poly.h
@@ -104,10 +104,10 @@ int sgn(T val) {
 // Count the positive roots of a polynomial using Sturm's theorem.
 template <typename T>
 int polycountroots (const Eigen::Matrix<T, Eigen::Dynamic, 1>& p) {
+  if (p.rows() <= 1) return 0;
+
   int n = p.rows() - 1,
       count = 0;
-
-  if (n <= 0) return 0;
 
   // Compute the initial signs and count any initial sign change.
   Eigen::Matrix<T, Eigen::Dynamic, 1> p0 = p, p1 = polyder(p0), tmp;
