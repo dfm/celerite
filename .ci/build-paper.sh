08c39a45f256475f71cd58afae8b65194a855fac
Dan F-M <danfm@nyu.edu>
2017-03-29 10:47:46 -0700

    skeleton of second paper

--- .ci/build-paper.sh
+++ .ci/build-paper.sh
@@ -13,7 +13,8 @@ if [ -n "$GITHUB_API_KEY" ]; then
   cd $TRAVIS_BUILD_DIR
   git checkout --orphan $TRAVIS_BRANCH-pdf
   git rm -rf .
-  git add -f paper/ms.pdf
-  git -c user.name='travis' -c user.email='travis' commit -m "making a paper"
+  git add -f paper/paper1/ms.pdf
+  git add -f paper/paper2/celerite2.pdf
+  git -c user.name='travis' -c user.email='travis' commit -m "building the papers"
   git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
 fi
