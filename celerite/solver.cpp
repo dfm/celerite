6fdf693013d5eab8b645853dc828f16c51c92261
Dan F-M <danfm@nyu.edu>
2017-04-07 10:18:46 -0400

    slightly faster real cholesky

--- celerite/solver.cpp
+++ celerite/solver.cpp
@@ -66,33 +66,29 @@ public:
 
   auto serialize () const {
     return std::make_tuple(
-      this->computed_, this->n_, this->J_real_, this->J_comp_, this->log_det_,
-      this->phi_real_, this->u_real_, this->X_real_,
-      this->phi_comp_, this->u1_comp_, this->u2_comp_, this->X1_comp_, this->X2_comp_,
+      this->computed_, this->n_, this->J1_, this->J2_, this->log_det_,
+      this->phi1_, this->phi2_, this->u1_, this->u2_, this->X1_, this->X2_,
       this->D_
     );
   };
 
   void deserialize (
-      bool computed, int n, int J_real, int J_comp, double log_det,
-      Eigen::MatrixXd phi_real, Eigen::VectorXd u_real, Eigen::MatrixXd X_real,
-      Eigen::MatrixXd phi_comp, Eigen::MatrixXd u1_comp, Eigen::MatrixXd u2_comp,
-      Eigen::MatrixXd X1_comp, Eigen::MatrixXd X2_comp,
+      bool computed, int n, int J1, int J2, double log_det,
+      Eigen::MatrixXd phi1, Eigen::MatrixXd phi2,
+      Eigen::MatrixXd u1, Eigen::MatrixXd u2,
+      Eigen::MatrixXd X1, Eigen::MatrixXd X2,
       Eigen::VectorXd D) {
     this->computed_ = computed;
     this->n_        = n;
-    this->N_        = n;
-    this->J_real_   = J_real;
-    this->J_comp_   = J_comp;
+    this->J1_       = J1;
+    this->J2_       = J2;
     this->log_det_  = log_det;
-    this->u_real_   = u_real;
-    this->phi_real_ = phi_real;
-    this->X_real_   = X_real;
-    this->phi_comp_ = phi_comp;
-    this->u1_comp_  = u1_comp;
-    this->u2_comp_  = u2_comp;
-    this->X1_comp_  = X1_comp;
-    this->X2_comp_  = X2_comp;
+    this->phi1_     = phi1;
+    this->phi2_     = phi2;
+    this->u1_       = u1;
+    this->u2_       = u2;
+    this->X1_       = X1;
+    this->X2_       = X2;
     this->D_        = D;
   };
 };
@@ -1006,7 +1002,7 @@ Returns:
   });
 
   cholesky_solver.def("__setstate__", [](PicklableCholeskySolver& solver, py::tuple t) {
-    if (t.size() != 14) throw std::runtime_error("Invalid state!");
+    if (t.size() != 12) throw std::runtime_error("Invalid state!");
     new (&solver) PicklableCholeskySolver();
     solver.deserialize(
       t[0].cast<bool>(),
@@ -1016,18 +1012,14 @@ Returns:
       t[4].cast<double>(),
 
       t[5].cast<Eigen::MatrixXd>(),
-      t[6].cast<Eigen::VectorXd>(),
+      t[6].cast<Eigen::MatrixXd>(),
       t[7].cast<Eigen::MatrixXd>(),
-
       t[8].cast<Eigen::MatrixXd>(),
       t[9].cast<Eigen::MatrixXd>(),
       t[10].cast<Eigen::MatrixXd>(),
-      t[11].cast<Eigen::MatrixXd>(),
-      t[12].cast<Eigen::MatrixXd>(),
 
-      t[13].cast<Eigen::VectorXd>()
+      t[11].cast<Eigen::VectorXd>()
     );
-
   });
 
   return m.ptr();
--- cpp/include/celerite/solver/cholesky.h
+++ cpp/include/celerite/solver/cholesky.h
@@ -37,87 +37,74 @@ int compute (
   if (a_comp.rows() != c_comp.rows()) return SOLVER_DIMENSION_MISMATCH;
   if (a_comp.rows() != d_comp.rows()) return SOLVER_DIMENSION_MISMATCH;
 
-  this->n_ = N_ = x.rows();
-  J_real_ = a_real.rows();
-  J_comp_ = a_comp.rows();
-  //Eigen::Array<T, Eigen::Dynamic, 1> a(J_comp_), b(J_comp_), c(J_comp_), d(J_comp_);
-  //a << a_comp, a_real;
-  //b.setConstant(T(0.0));
-  //b.head(b_comp.rows()) << b_comp;
-  //c << c_comp, c_real;
-  //d.setConstant(T(0.0));
-  //d.head(b_comp.rows()) << d_comp;
+  int N = this->n_ = x.rows(), J0 = a_real.rows();
+  J1_ = a_real.rows() + a_comp.rows();
+  J2_ = a_comp.rows();
+  Eigen::Array<T, Eigen::Dynamic, 1> a1(J1_), a2(J2_), b1(J1_), b2(J2_), c1(J1_), c2(J2_), d1(J1_), d2(J2_), cd1(J1_), sd2;
+  a1 << a_comp, a_real;
+  a2 << a_comp;
+  b1.setConstant(T(0.0));
+  b1.head(J2_) << b_comp;
+  b2 << b_comp;
+  c1 << c_comp, c_real;
+  c2 << c_comp;
+  d1.setConstant(T(0.0));
+  d1.head(J2_) << d_comp;
+  d2 << d_comp;
 
-  T a_sum = a_real.sum() + a_comp.sum();
-
-  phi_real_.resize(J_real_, N_-1);
-  u_real_ = a_real.array();
-  X_real_.resize(J_real_, N_);
-
-  phi_comp_.resize(J_comp_, N_-1);
-  u1_comp_.resize(J_comp_, N_-1);
-  u2_comp_.resize(J_comp_, N_-1);
-  X1_comp_.resize(J_comp_, N_);
-  X2_comp_.resize(J_comp_, N_);
-
-  D_.resize(N_);
+  T a_sum = a1.sum();
+  phi1_.resize(J1_, N-1);
+  phi2_.resize(J2_, N-1);
+  u1_.resize(J1_, N-1);
+  u2_.resize(J2_, N-1);
+  X1_.resize(J1_, N);
+  X2_.resize(J2_, N);
+  D_.resize(N);
 
   // Work arrays.
-  Eigen::Matrix<T, Eigen::Dynamic, 1> tmp00, tmp01, tmp10, tmp02, tmp20, tmp11, tmp12, tmp21, tmp22;
-  Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> S00(J_real_, J_real_), S01, S02, S11, S12, S22;
+  Eigen::Matrix<T, Eigen::Dynamic, 1> tmp11, tmp12, tmp21, tmp22;
+  Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> S11, S12, S22;
 
   // First row
   D_(0) = diag(0) + a_sum;
-  X_real_.col(0).setConstant(1.0 / D_(0));
-  X1_comp_.col(0) = cos(d_comp.array() * x(0)) / D_(0);
-  X2_comp_.col(0) = sin(d_comp.array() * x(0)) / D_(0);
+  X1_.col(0) = cos(d1 * x(0)) / D_(0);
+  X2_.col(0) = sin(d2 * x(0)) / D_(0);
 
-  S00.setConstant(1.0 / D_(0));
-  S01 = D_(0) * X_real_.col(0) * X1_comp_.col(0).transpose();
-  S02 = D_(0) * X_real_.col(0) * X2_comp_.col(0).transpose();
-  S11 = D_(0) * X1_comp_.col(0) * X1_comp_.col(0).transpose();
-  S12 = D_(0) * X1_comp_.col(0) * X2_comp_.col(0).transpose();
-  S22 = D_(0) * X2_comp_.col(0) * X2_comp_.col(0).transpose();
+  S11 = D_(0) * X1_.col(0) * X1_.col(0).transpose();
+  S12 = D_(0) * X1_.col(0) * X2_.col(0).transpose();
+  S22 = D_(0) * X2_.col(0) * X2_.col(0).transpose();
 
-  for (int n = 1; n < N_; ++n) {
-    u1_comp_.col(n-1) = a_comp.array() * cos(d_comp.array() * x(n)) + b_comp.array() * sin(d_comp.array() * x(n));
-    u2_comp_.col(n-1) = a_comp.array() * sin(d_comp.array() * x(n)) - b_comp.array() * cos(d_comp.array() * x(n));
+  cd1.setConstant(T(1.0));
+  for (int n = 1; n < N; ++n) {
+    cd1.head(J2_) = cos(d2*x(n));
+    sd2 = sin(d2*x(n));
 
-    phi_real_.col(n-1) = exp(-c_real.array() * (x(n) - x(n-1)));
-    phi_comp_.col(n-1) = exp(-c_comp.array() * (x(n) - x(n-1)));
+    u1_.col(n-1).head(J2_) = a2 * cd1.head(J2_) + b2 * sd2;
+    u1_.col(n-1).tail(J0) = a1.tail(J0);
+    u2_.col(n-1) = a2 * sd2 - b2 * cd1.head(J2_);
+    phi2_.col(n-1) = exp(-c2 * (x(n) - x(n-1)));
+    phi1_.col(n-1).head(J2_) = phi2_.col(n-1);
+    phi1_.col(n-1).tail(J0) = exp(-c1.tail(J0) * (x(n) - x(n-1)));
 
-    S00 = phi_real_.col(n-1).asDiagonal() * S00 * phi_real_.col(n-1).asDiagonal();
-    S01 = phi_real_.col(n-1).asDiagonal() * S01 * phi_comp_.col(n-1).asDiagonal();
-    S02 = phi_real_.col(n-1).asDiagonal() * S02 * phi_comp_.col(n-1).asDiagonal();
-    S11 = phi_comp_.col(n-1).asDiagonal() * S11 * phi_comp_.col(n-1).asDiagonal();
-    S12 = phi_comp_.col(n-1).asDiagonal() * S12 * phi_comp_.col(n-1).asDiagonal();
-    S22 = phi_comp_.col(n-1).asDiagonal() * S22 * phi_comp_.col(n-1).asDiagonal();
+    S11 = phi1_.col(n-1).asDiagonal() * S11 * phi1_.col(n-1).asDiagonal();
+    S12 = phi1_.col(n-1).asDiagonal() * S12 * phi2_.col(n-1).asDiagonal();
+    S22 = phi2_.col(n-1).asDiagonal() * S22 * phi2_.col(n-1).asDiagonal();
 
-    tmp00 = u_real_.transpose() * S00;
-    tmp01 = u_real_.transpose() * S01;
-    tmp10 = (S01 * u1_comp_.col(n-1)).transpose();
-    tmp02 = u_real_.transpose() * S02;
-    tmp20 = (S02 * u2_comp_.col(n-1)).transpose();
-    tmp11 = u1_comp_.col(n-1).transpose() * S11;
-    tmp12 = u1_comp_.col(n-1).transpose() * S12;
-    tmp21 = (S12 * u2_comp_.col(n-1)).transpose();
-    tmp22 = u2_comp_.col(n-1).transpose() * S22;
+    tmp11 = u1_.col(n-1).transpose() * S11;
+    tmp12 = u1_.col(n-1).transpose() * S12;
+    tmp21 = (S12 * u2_.col(n-1)).transpose();
+    tmp22 = u2_.col(n-1).transpose() * S22;
 
     D_(n) = diag(n) + a_sum
-      - (tmp00 + 2.0 * (tmp10 + tmp20)).transpose() * u_real_
-      - tmp11.transpose() * u1_comp_.col(n-1)
-      - (2.0 * tmp12 + tmp22).transpose() * u2_comp_.col(n-1);
+      - tmp11.transpose() * u1_.col(n-1)
+      - (2.0 * tmp12 + tmp22).transpose() * u2_.col(n-1);
 
-    X_real_.col(n)  = (1.0 - (tmp00 + tmp10 + tmp20).array()) / D_(n);
-    X1_comp_.col(n) = (cos(d_comp.array()*x(n)) - (tmp01 + tmp11 + tmp21).array()) / D_(n);
-    X2_comp_.col(n) = (sin(d_comp.array()*x(n)) - (tmp02 + tmp12 + tmp22).array()) / D_(n);
+    X1_.col(n) = (cd1 - (tmp11 + tmp21).array()) / D_(n);
+    X2_.col(n) = (sd2 - (tmp12 + tmp22).array()) / D_(n);
 
-    S00 = S00 + D_(n) * X_real_.col(n) * X_real_.col(n).transpose();
-    S01 = S01 + D_(n) * X_real_.col(n) * X1_comp_.col(n).transpose();
-    S02 = S02 + D_(n) * X_real_.col(n) * X2_comp_.col(n).transpose();
-    S11 = S11 + D_(n) * X1_comp_.col(n) * X1_comp_.col(n).transpose();
-    S12 = S12 + D_(n) * X1_comp_.col(n) * X2_comp_.col(n).transpose();
-    S22 = S22 + D_(n) * X2_comp_.col(n) * X2_comp_.col(n).transpose();
+    S11 = S11 + D_(n) * X1_.col(n) * X1_.col(n).transpose();
+    S12 = S12 + D_(n) * X1_.col(n) * X2_.col(n).transpose();
+    S22 = S22 + D_(n) * X2_.col(n) * X2_.col(n).transpose();
   }
 
   this->log_det_ = log(D_.array()).sum();
@@ -131,32 +118,28 @@ void solve (const Eigen::MatrixXd& b, T* x) const {
   if (!(this->computed_)) throw compute_exception();
 
   int nrhs = b.cols();
-  Eigen::Matrix<T, Eigen::Dynamic, 1> f0(J_real_), f1(J_comp_), f2(J_comp_);
-  Eigen::Map<Eigen::Array<T, Eigen::Dynamic, Eigen::Dynamic> > xout(x, N_, nrhs);
+  Eigen::Matrix<T, Eigen::Dynamic, 1> f1(J1_), f2(J2_);
+  Eigen::Map<Eigen::Array<T, Eigen::Dynamic, Eigen::Dynamic> > xout(x, this->n_, nrhs);
 
   for (int k = 0; k < nrhs; ++k) {
     // Forward
-    f0.setConstant(T(0.0));
     f1.setConstant(T(0.0));
     f2.setConstant(T(0.0));
     xout(0, k) = b(0, k);
-    for (int n = 1; n < N_; ++n) {
-      f0 = phi_real_.col(n-1).asDiagonal() * (f0 + X_real_.col(n-1) * xout(n-1, k));
-      f1 = phi_comp_.col(n-1).asDiagonal() * (f1 + X1_comp_.col(n-1) * xout(n-1, k));
-      f2 = phi_comp_.col(n-1).asDiagonal() * (f2 + X2_comp_.col(n-1) * xout(n-1, k));
-      xout(n, k) = b(n, k) - u_real_.transpose() * f0 - u1_comp_.col(n-1).transpose() * f1 - u2_comp_.col(n-1).transpose() * f2;
+    for (int n = 1; n < this->n_; ++n) {
+      f1 = phi1_.col(n-1).asDiagonal() * (f1 + X1_.col(n-1) * xout(n-1, k));
+      f2 = phi2_.col(n-1).asDiagonal() * (f2 + X2_.col(n-1) * xout(n-1, k));
+      xout(n, k) = b(n, k) - u1_.col(n-1).transpose() * f1 - u2_.col(n-1).transpose() * f2;
     }
     xout.col(k) /= D_.array();
 
     // Backward
-    f0.setConstant(T(0.0));
     f1.setConstant(T(0.0));
     f2.setConstant(T(0.0));
-    for (int n = N_-2; n >= 0; --n) {
-      f0 = phi_real_.col(n).asDiagonal() * (f0 + u_real_ * xout(n+1, k));
-      f1 = phi_comp_.col(n).asDiagonal() * (f1 + u1_comp_.col(n) * xout(n+1, k));
-      f2 = phi_comp_.col(n).asDiagonal() * (f2 + u2_comp_.col(n) * xout(n+1, k));
-      xout(n, k) = xout(n, k) - X_real_.col(n).transpose() * f0 - X1_comp_.col(n).transpose() * f1 - X2_comp_.col(n).transpose() * f2;
+    for (int n = this->n_-2; n >= 0; --n) {
+      f1 = phi1_.col(n).asDiagonal() * (f1 + u1_.col(n) * xout(n+1, k));
+      f2 = phi2_.col(n).asDiagonal() * (f2 + u2_.col(n) * xout(n+1, k));
+      xout(n, k) = xout(n, k) - X1_.col(n).transpose() * f1 - X2_.col(n).transpose() * f2;
     }
   }
 };
@@ -176,79 +159,11 @@ void solve (const Eigen::MatrixXd& b, T* x) const {
   using Solver<T>::solve;
 
 protected:
-  int N_, J_real_, J_comp_;
-  Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>
-    phi_real_, X_real_,
-    u1_comp_, u2_comp_, phi_comp_, X1_comp_, X2_comp_;
-  Eigen::Matrix<T, Eigen::Dynamic, 1> D_, u_real_;
+  int J1_, J2_;
+  Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> u1_, u2_, phi1_, phi2_, X1_, X2_;
+  Eigen::Matrix<T, Eigen::Dynamic, 1> D_;
 
 };
-
-//template <typename T>
-//int CholeskySolver<T>::compute (
-//    const Eigen::Matrix<T, Eigen::Dynamic, 1>& a_real,
-//    const Eigen::Matrix<T, Eigen::Dynamic, 1>& c_real,
-//    const Eigen::Matrix<T, Eigen::Dynamic, 1>& a_comp,
-//    const Eigen::Matrix<T, Eigen::Dynamic, 1>& b_comp,
-//    const Eigen::Matrix<T, Eigen::Dynamic, 1>& c_comp,
-//    const Eigen::Matrix<T, Eigen::Dynamic, 1>& d_comp,
-//    const Eigen::VectorXd& x,
-//    const Eigen::Matrix<T, Eigen::Dynamic, 1>& diag
-//)
-//{
-//  this->computed_ = false;
-//  if (x.rows() != diag.rows()) return SOLVER_DIMENSION_MISMATCH;
-//  if (alpha_real.rows() != beta_real.rows()) return SOLVER_DIMENSION_MISMATCH;
-//  if (alpha_complex_real.rows() != alpha_complex_imag.rows()) return SOLVER_DIMENSION_MISMATCH;
-//  if (alpha_complex_real.rows() != beta_complex_real.rows()) return SOLVER_DIMENSION_MISMATCH;
-//  if (alpha_complex_real.rows() != beta_complex_imag.rows()) return SOLVER_DIMENSION_MISMATCH;
-
-//  int N = x.rows(), J = alpha_real.rows() + 2 * alpha_complex_real.rows();
-//  phi_.resize(J, N-1);
-//  D_.resize(N);
-//  X_.resize(J, N);
-//  alpha_.resize(J);
-//  Eigen::Array<std::complex<T>, Eigen::Dynamic, Eigen::Dynamic> S(J, J);
-//  Eigen::Array<std::complex<T>, Eigen::Dynamic, 1> beta(J), tmp(J);
-
-//  int i = 0;
-//  for (int j = 0; j < alpha_real.rows(); ++j) {
-//    alpha_(i) = alpha_real(j);
-//    beta(i++) = beta_real(j);
-//  }
-//  for (int j = 0; j < alpha_complex_real.rows(); ++j) {
-//    alpha_(i) = 0.5*std::complex<T>(alpha_complex_real(j), alpha_complex_imag(j));
-//    beta(i++) = std::complex<T>(beta_complex_real(j), beta_complex_imag(j));
-//    alpha_(i) = 0.5*std::complex<T>(alpha_complex_real(j), -alpha_complex_imag(j));
-//    beta(i++) = std::complex<T>(beta_complex_real(j), -beta_complex_imag(j));
-//  }
-
-//  // First pass
-//  T alpha_sum = alpha_.sum().real();
-//  D_(0) = diag(0) + alpha_sum;
-//  S.setConstant(1.0 / D_(0));
-//  X_.col(0).setConstant(1.0 / D_(0));
-
-//  for (int n = 1; n < N; ++n) {
-//    phi_.col(n-1) = exp(-beta*(x(n) - x(n-1)));
-//    S *= (phi_.col(n-1) * phi_.col(n-1).transpose()).array();
-//    for (int k = 0; k < J; ++k)
-//      tmp(k) = (alpha_ * S.col(k)).sum();
-//    D_(n) = (diag(n) + alpha_sum - (alpha_ * tmp).sum()).real();
-//    if (D_(n) < T(0.0)) return -n;
-//    //D_(n) = sqrt(D_(n));
-//    X_.col(n).array() = (1.0 - tmp) / D_(n);
-//    S += D_(n) * (X_.col(n) * X_.col(n).transpose()).array();
-//  }
-
-//  this->j_ = J;
-//  this->n_ = N;
-//  this->log_det_ = log(D_).sum();
-//  this->computed_ = true;
-
-//  return 0;
-//}
-
 
 //template <typename T>
 //Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> BandSolver<T>::dot (
