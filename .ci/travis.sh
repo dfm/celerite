b556c2b45bd5c7c07909b024a76162bf5a6a74be
Dan F-M <danfm@nyu.edu>
2017-04-28 09:09:40 -0700

    trying again

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -54,20 +54,19 @@ conda install -c conda-forge numpy=$NUMPY_VERSION setuptools pytest pybind11 $AU
 
 if [[ $AUTODIFF_LIBRARY == stan ]]
 then
-  echo "hi"
   mkdir -p stan
   cd stan
   wget "https://github.com/stan-dev/math/archive/v2.15.0.tar.gz"
   tar -xf v2.15.0.tar.gz --strip-components 1
   cd ..
 
-  export BUILD_ARGS="\"-Istan/stan -Istan/lib/boost_1.62.0 -Istan/lib/cvodes_2.9.0/include -DUSE_STAN_MATH\""
-  echo "wtf"
+  CXX=g++-4.8 CC=gcc-4.8 python setup.py build_ext "-Istan/stan -Istan/lib/boost_1.62.0 -Istan/lib/cvodes_2.9.0/include -DUSE_STAN_MATH" install
+  return
 fi
 
 # Build the extension
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  python setup.py build_ext "$BUILD_ARGS" install
+  python setup.py install
 else
   CXX=g++-4.8 CC=gcc-4.8 python setup.py build_ext $BUILD_ARGS install
 fi
