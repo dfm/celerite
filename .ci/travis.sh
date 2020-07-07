a7863b0651b9fc1bb7488b5837aeb72fb883314e
Dan F-M <foreman.mackey@gmail.com>
2020-07-07 08:25:59 -0400

    numpy versions

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -36,9 +36,9 @@ fi
 
 # http://conda.pydata.org/docs/travis.html#the-travis-yml-file
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
+  wget --quiet https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
 else
-  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+  wget --quiet https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 fi
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
