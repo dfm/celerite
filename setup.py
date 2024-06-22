18c3b5ca3be837f9c9df6227a8940731884542a5
Dan Foreman-Mackey <foreman.mackey@gmail.com>
2024-06-22 07:18:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ setup(
     install_requires=["numpy"],
     extras_require={
         "test": [
-            "autograd",
             "coverage[toml]",
             "pytest",
             "pytest-cov",
